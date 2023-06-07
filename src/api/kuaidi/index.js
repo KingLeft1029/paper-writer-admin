f7e28deb2d92e353e37f28621ebec0159ab06430
KingLeft1029 <1543670894@qq.com>
2023-06-07 17:59:31 +0800

    tj

--- src/api/kuaidi/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getDeliveryMap(query) {
-  return request({
-    url: '/kuaidi100/getDeliveryMapView',
-    method: 'get',
-    params: query
-  })
-}
--- src/mixin.js
+++
@@ -1,7 +0,0 @@
-export const mixin = {
-  computed: {
-    apiModelProperty() {
-
-    }
-  }
-}
